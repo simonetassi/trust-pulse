a5fffeafa28a1a9dd3335b59a44c76c04ab45dad
simonetassi <72198236+simonetassi@users.noreply.github.com>
2026-03-11 11:23:33 +0100

    chore: initialize hardhat project
    Fix #1

---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.28",
+
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL ?? "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY ?? ""
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6"
+  }
+};
+
+export default config;
