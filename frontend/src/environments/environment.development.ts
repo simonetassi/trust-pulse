fc27b2d7321092e3cee68545e74119174d5906dc
simonetassi <72198236+simonetassi@users.noreply.github.com>
2026-03-19 18:50:12 +0100

    feat(frontend): setup angular project

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  wsUrl: 'http://localhost:3000',
+  network: 'localhost',
+  hardhatChainId: 31337,
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: '',
+  wsUrl: '',
+  network: 'sepolia',
+  hardhatChainId: 1,
+};
