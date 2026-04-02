851d6d927522ffe600cfadf3751712292ac211e9
simonetassi <72198236+simonetassi@users.noreply.github.com>
2026-04-02 19:49:11 +0200

    feat(multi-oracle): refactor backend structure

--- backend/src/api/routes/devices.ts
+++ backend/src/api/routes/devices.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { getContract } from "../contractProvider";
+import { getContract } from "../../shared/contractProvider";
 
 export const devicesRouter = Router();
 
